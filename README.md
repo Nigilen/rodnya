e732a049caee8d6d2fe195055de7a09aade031fb
Roman Shmarov <romashmarov82@gmail.com>
2024-06-11 13:52:10 +0300

    feat: assets downloading & config

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Rodnya
 
-3д-анимация кбубка для сайта rodnya. Сборка на vite. Все работает на Three.js
+3д-анимация клубка для сайта rodnya. Сборка на vite. Все работает на Three.js
 
 Тестовый сайт: [https://front.friendlee.pro/rodnya/](https://front.friendlee.pro/rodnya/).
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,25 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-    build: {
-        chunkSizeWarningLimit: 1000,
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    three: ['three']
-                }
-            },
-
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+    return defineConfig({
+        base: env.VITE_BASE_URL,
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            }
         },
-    },
-})
+        build: {
+            chunkSizeWarningLimit: 1000,
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        three: ['three']
+                    }
+                },
+    
+            },
+        },
+    });
+}
