79e5175529cd57f334cf9c6fbd21d67b26c85ac7
nigilen <you@example.com>
2024-10-09 15:10:40 +0200

    fix: somefix

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,32 +1,32 @@
 import { API_URL } from "../utils/config";
 
 export const getContacts = async () => {
-  return await fetch(`${API_URL}/contacts`, { next: {revalidate: 3000 }})
+  return await fetch(`${API_URL}/contacts`, { next: {revalidate: 20 }})
     .then(response => response.json());
 };
 
 export const getServices = async () => {
-  return await fetch(`${API_URL}/services`, { next: {revalidate: 3000 }})
+  return await fetch(`${API_URL}/services`, { next: {revalidate: 20 }})
     .then(response => response.json());
 };
 
 export const getTeam = async () => {
-  return await fetch(`${API_URL}/team`, { next: {revalidate: 3000 }})
+  return await fetch(`${API_URL}/team`, { next: {revalidate: 20 }})
     .then(response => response.json());
 };
 
 export const getSocials = async () => {
-  return await fetch(`${API_URL}/socials`, { next: {revalidate: 3000 }})
+  return await fetch(`${API_URL}/socials`, { next: {revalidate: 20 }})
     .then(response => response.json());
 };
 
 export const getProjects = async () => {
-  return await fetch(`${API_URL}/projects`, { next: {revalidate: 3000 }})
+  return await fetch(`${API_URL}/projects`, { next: {revalidate: 20 }})
     .then(response => response.json());
 };
 
 export const getSingleProject = async (slug: string) => {
-  return await fetch(`${API_URL}/projects/${slug}`, { next: {revalidate: 3000 }})
+  return await fetch(`${API_URL}/projects/${slug}`, { next: {revalidate: 20 }})
     .then(response => response.json());
 };
 
