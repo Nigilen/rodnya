86071e0390b9b1e755332b5203a30fd2813e4d61
nigilen <you@example.com>
2024-10-11 22:04:09 +0200

    fix(#11): fix bg video about page for IOS

--- src/components/sections/hero/hero.tsx
+++ src/components/sections/hero/hero.tsx
@@ -16,7 +16,7 @@ export const Hero: FC<HeroProps> = ({title, description, background, video}) => 
       className={cn(styles.hero, background && [styles.hero__with_bg], video && [styles.hero__with_bg])} 
       style={{ backgroundImage: `url(${background})` }}
     >
-      <video className={styles.hero_video} width="auto" height="100%" preload="true" autoPlay muted loop>
+      <video className={styles.hero_video} width="auto" height="100%" preload="true" autoPlay muted loop playsInline>
         <source src={video} type="video/webm" />
       </video>
       <div className={cn(styles.hero_content, background && [styles.hero_content__proportional] || video && [styles.hero_content__proportional])}>
