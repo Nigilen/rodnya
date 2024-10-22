add8c3842a7cab9a9edd19b3cf37cb659af9a296
ivankprod <houseprotector@mail.ru>
2024-10-22 15:00:52 +0500

    feat(#11): bug #78 fixed

--- src/components/sections/case-content/case-video/case-video.tsx
+++ src/components/sections/case-content/case-video/case-video.tsx
@@ -1,7 +1,10 @@
-'use client'
+"use client";
 
-import { FC, useState } from 'react';
-import styles from './case-video.module.css';
+import { FC, useState } from "react";
+import Modal from "@/src/components/layout/modal";
+import { CloseIcon } from "@/src/components/svg/close-icon/close-icon";
+
+import styles from "./case-video.module.css";
 
 type Props = {
   video: string;
@@ -10,20 +13,51 @@ type Props = {
 
 export const CaseVideo: FC<Props> = ({ video, preview }) => {
   const [isActive, setIsActive] = useState(false);
-  
+
   return (
-    <div className={styles.video} style={{ backgroundImage: `linear-gradient(to bottom, black -50%, transparent 30%), url(${preview})` }}>
-      {!isActive && <button className={styles.play} type='button' onClick={() => setIsActive(!isActive)}>
-        <img src="/play.svg" alt="play" />
-      </button>
-      }
-      {isActive && <iframe
-        width="100%"
-        height="100%"
-        src={video}
-        allow="fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />}
+    <div
+      className={styles.video}
+      style={{
+        backgroundImage: `linear-gradient(to bottom, black -50%, transparent 30%), url(${preview})`,
+      }}
+    >
+      {!isActive && (
+        <button
+          className={styles.play}
+          type="button"
+          onClick={() => setIsActive(!isActive)}
+        >
+          <img src="/play.svg" alt="play" />
+        </button>
+      )}
+      <Modal
+        opened={isActive}
+        onClose={() => {
+          setIsActive(false);
+        }}
+        style={{ padding: "10%" }}
+      >
+        <iframe
+          width="100%"
+          height="100%"
+          src={video}
+          allow="fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          style={{ border: 0 }}
+        />
+        <button
+          style={{
+            position: "absolute",
+            marginLeft: -41,
+            marginTop: -12,
+          }}
+          onClick={() => {
+            setIsActive(false);
+          }}
+        >
+          <CloseIcon />
+        </button>
+      </Modal>
     </div>
   );
-}
+};
