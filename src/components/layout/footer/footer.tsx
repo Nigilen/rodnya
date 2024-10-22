fef2b4d8a3ba8fb86ae7d29dac64d365f40a1d7d
ivankprod <houseprotector@mail.ru>
2024-10-22 18:44:00 +0500

    feat(#11): policy added

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,30 +1,35 @@
-'use client'
+"use client";
 
-import { usePathname } from 'next/navigation';
-import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "./footer.module.css";
 import { FC } from "react";
-import { FooterMainPage } from './footer-main-page/footer-main-page';
-
+import { FooterMainPage } from "./footer-main-page/footer-main-page";
 
 export const Footer: FC = ({}) => {
   const pathname = usePathname();
 
   return (
     <>
-      {pathname !== '/' ? 
+      {pathname !== "/" ? (
         <footer className={styles.footer}>
           <div>
             <section className={styles.footer_bottom}>
               <p>ООО&nbsp;«Креативные&nbsp;ПИАР-решения»</p>
-              <Link href="/privacy" className="privacy">Политика конфиденциальности</Link>
+              <a
+                target="_blank"
+                rel="noopener"
+                href="/rodnya_policy.docx"
+                className="privacy"
+              >
+                Политика конфиденциальности
+              </a>
               <p>©2024</p>
             </section>
           </div>
         </footer>
-      : 
-      <FooterMainPage />
-      }
+      ) : (
+        <FooterMainPage />
+      )}
     </>
-  )
-}
+  );
+};
